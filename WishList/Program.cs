1219651d0b2b4e7c3ccbc4b05bb571b5e151e99f
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:38:01 +0200

    first commit

---
+++ WishList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wishes = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("What do you wish for?");
+                wishes.Add(Console.ReadLine());
+
+
+            }
+
+            string filePath = @"C:\Users\opilane\Data\wishtlist.txt";
+            File.WriteAllLines(filePath, wishes);
+
+
+
+
+        }
+    }
+}
